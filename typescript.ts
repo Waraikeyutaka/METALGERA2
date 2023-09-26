500f5f222653a2a3a7b1ea01594a5d60ddbbcdd1
Waraikeyutaka <waraikeyutaka@au.com>
2023-09-26 16:26:11 +0900

    作業成果
    途中

--- typescript.ts
+++ typescript.ts
@@ -1,43 +1,89 @@
-onload = function() {
-  draw();
+onload = function() { 
+  draw(100,100); 
+  draw(300,100);
 };
-
-function draw() {
+var cvs
+var xx=370
+function draw(x:number,y:number) {
   var canvas:HTMLCanvasElement = document.getElementById('rectangle') as HTMLCanvasElement;
   if ( ! canvas || ! canvas.getContext ) {
     return false;
   }
-  var cvs = canvas.getContext('2d');
+  cvs = canvas.getContext('2d');
 
   if(cvs==null){return}
-  /* rectangle */
-  cvs.beginPath(); /* 図形を描き始めることを宣言 */
-  cvs.moveTo(50, 50); /* 図形の描き始めを移動 */
-  cvs.lineTo(150, 50); /* 図形の線の終わりを決める */
-  cvs.lineTo(150, 150);
-  cvs.lineTo(50, 150);
-  cvs.closePath(); /* 描いた線を閉じる */
-  cvs.stroke(); /* 描いた図形を線で表示させる */
-}
-
-onload = function() {
-  draw1();
+  const chara1 = new Image();
+chara1.src = "./棒人間素材.png";  // 画像のURLを指定
+chara1.onload = () => {
+  if(cvs==null){return}
+  cvs.drawImage(chara, 170, 80,50,50);
 };
 
-function draw1() {
-  var canvas:HTMLCanvasElement = document.getElementById('rectangle') as HTMLCanvasElement;
-  if ( ! canvas || ! canvas.getContext ) {
-    return false;
-  }
-  var cvs = canvas.getContext('2d');
 
   if(cvs==null){return}
+  const chara = new Image();
+chara.src = "./棒人間素材.png";  // 画像のURLを指定
+chara.onload = () => {
+  if(cvs==null){return}
+  cvs.drawImage(chara, 370, 270,50,50);
+};
+
+window.addEventListener(
+  "keydown",
+  (event) => {
+    if (event.defaultPrevented) {
+      return; // 既定のアクションがキャンセルされている場合は何もしないようにします。
+    }
+
+    let handled = false;
+    if (event.key !== undefined) {
+      // KeyboardEvent.key でイベントを処理し、handled を true に設定します。
+      handled = true;
+    } else if (event.keyCode !== undefined) {
+      // KeyboardEvent.keyCode でイベントを処理し、handled を true に設定します。
+      handled = true;
+    }
+
+    if (handled) {
+      // イベントが処理された場合、"ダブルアクション" を抑制する
+      event.preventDefault();
+      console.log("aaaa")
+      if(event.keyCode == 37) {
+        cvs.fillStyle = "white";
+        cvs.fillRect(0, 0, 1200,1200)
+       xx-=10
+        console.log("left")
+        cvs.drawImage(chara, xx, 270,50,50);
+        cvs.drawImage(chara, 170, 80,50,50);
+        draw(100,100); 
+        draw(300,100);
+      
+        
+      }
+      if(event.keyCode == 39) {
+        cvs.fillStyle = "white";
+        cvs.fillRect(0, 0, 1200,1200)
+       xx+=10
+        console.log("left")
+        cvs.drawImage(chara, xx, 270,50,50);
+        cvs.drawImage(chara, 170, 80,50,50);
+        draw(100,100); 
+        draw(300,100);
+      
+        
+      }
+
+    }
+  },
+  true,
+);
+
   /* rectangle */
   cvs.beginPath(); /* 図形を描き始めることを宣言 */
-  cvs.moveTo(50, 50); /* 図形の描き始めを移動 */
-  cvs.lineTo(150, 50); /* 図形の線の終わりを決める */
-  cvs.lineTo(150, 150);
-  cvs.lineTo(50, 150);
+  cvs.moveTo(50+x, 50+y); /* 図形の描き始めを移動 */
+  cvs.lineTo(150+x, 50+y); /* 図形の線の終わりを決める */
+  cvs.lineTo(150+x, 150+y);
+  cvs.lineTo(50+x, 150+y);
   cvs.closePath(); /* 描いた線を閉じる */
   cvs.stroke(); /* 描いた図形を線で表示させる */
 }
